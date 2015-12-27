bcc68cc8319cb0fd9787e7a6227d3aea2d14fc7d
freakiemania <freakiemania@gmail.com>
2015-12-26 20:52:21 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## begins with setting the mean called specialnumber <- NULL
+## set <- function(y) {x <<- y; specialnumber <<- NULL} is a function to set the x to a y, and resets the mean, specialnumber, to NULL
+## get <- function() x returns the x
+## setmean <- function(mean) specialnumber <<- mean sets the mean, specialnumber, to mean
+## getmean <- function() specialnumber returns the mean, specialnumber
+## list(set = set, get = get,setmean = setmean,getmean = getmean) returns the 'special vector' 
+## containing all of the functions just defined
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+specialnumber <- NULL
+set <- function(y) {
+  x <<- y
+  specialnumber <<- NULL
+}
+get <- function() x
+setmean <- function(mean) specialnumber <<- mean
+getmean <- function() specialnumber
+list(set = set, get = get,
+     setmean = setmean,
+     getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of the special "matrix" returned by makeCacheMatrix above
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getmean()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- mean(data, ...)
+  x$setmean(inverse)
+  inverse
 }
